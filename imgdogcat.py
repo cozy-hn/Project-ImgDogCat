d061a17956347be251bff4c9c154f8a0ded5ab36
cozy-hn <jhk2721@gmail.com>
2024-03-24 04:06:03 +0900

    Add imgdogcat.py : update tag randomnize

--- imgdogcat.py
+++ imgdogcat.py
@@ -17,7 +17,8 @@ parser.add_argument('-g', '--gif', action='store_true', help='Fetch a random gif
 args = parser.parse_args()
 
 if args.gif:
-    url = "https://api.giphy.com/v1/gifs/random?api_key=qYBdjc9BCKh4NLmuVgWiCakpWT3OraDz&tag=new+jeans&rating=g"
+    tags = ["new+jeans", "twice", "ive", "black+pink", "red+velvet", "itzy", "aespa"]
+    url = "https://api.giphy.com/v1/gifs/random?api_key=qYBdjc9BCKh4NLmuVgWiCakpWT3OraDz&tag=" + random.choice(tags) + "&rating=g"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
